2b2bfc4b82e42f4ade57cc61d01a44d85dc48746
Andrew <wormxexraped@gmail.com>
2017-02-08 21:40:14 +0100

    Groups and Grid
    Groups and grid first definition

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : Singleton<Grid> {
+
+	private Grid (){}
+
+
+	//Grid dimensions
+	public static int WEIGHT = 10;
+	public static int HEIGHT = 10;
+
+	// The grid is a bidimensional array of Transforms:
+
+	public static Transform [,] grid = new Transform[WEIGHT,HEIGHT];
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+
+	// Round vector method rounds any vector coordinates
+	public static Vector2 roundVector(Vector2 vect){
+		return new Vector2 (Mathf.Round(vect.x), Mathf.Round(vect.y));
+	}
+
+
+}
---
+++ Assets/Scripts/Group.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Group : MonoBehaviour {
+
+
+	private float lastFall = 0f;
+
+
+	public static int UPDATERATE = 1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time - lastFall >= UPDATERATE) {
+			transform.position = transform.position + Vector3.down;
+
+
+			lastFall = Time.time;
+		}
+
+	}
+}
